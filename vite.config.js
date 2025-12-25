6ad68ba60da04bdcb4ca42244d6e46924314e765
Dipak Shaaki <shanki.dipak@gmail.com>
2025-12-25 22:00:20 +0545

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Webi/', // GitHub Pages base URL
+    base: '/', // Base path for custom domain dipakshanki.com.np
     server: {
         port: 3000,
         open: true
